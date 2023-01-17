e3309991d77c912d0f37569bcc719a081d008a28
andrii <andriiholinko@gmail.com>
2023-01-17 19:30:25 +0100

    -updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       repo : 'git@github.com:Andrej-oss/Amazon.git',
       path : '/home/ec2-user',
       'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install --force && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_option' : 'forwardAgent=yes'
     }
