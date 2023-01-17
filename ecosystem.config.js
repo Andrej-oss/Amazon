6b94a6f06c575c279842986826430c48b88d02a3
andrii <andriiholinko@gmail.com>
2023-01-17 19:09:05 +0100

    -update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,11 @@ module.exports = {
   deploy : {
     production : {
       key  : 'ohio_key1.pem',
-      user : 'ubuntu',
+      user : 'ec2-user',
       host : '18.223.121.186',
       ref  : 'origin/main',
       repo : 'git@github.com:Andrej-oss/Amazon.git',
-      path : '/home/ubuntu',
+      path : '/home/ec2-user',
       'pre-deploy-local': '',
       'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
